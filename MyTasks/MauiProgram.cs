6d9880ebfda65197cdd4021c79fa0afdca5dbf78
Wilson S Leoputra <wilsonsuryajaya@gmail.com>
2024-08-25 21:04:21 +0800

    Added FontAwesome and create a rounded button borders

--- MyTasks/MauiProgram.cs
+++ MyTasks/MauiProgram.cs
@@ -14,6 +14,7 @@ namespace MyTasks
                 {
                     fonts.AddFont( "OpenSans-Regular.ttf", "OpenSansRegular" );
                     fonts.AddFont( "OpenSans-Semibold.ttf", "OpenSansSemibold" );
+                    fonts.AddFont( "fa-solid-900.ttf", "FASolid" );
                 } );
 
             builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
