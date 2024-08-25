d3601dea9d5bb85411a478366d8aff169c51dcdc
Wilson S Leoputra <wilsonsuryajaya@gmail.com>
2024-08-25 14:17:24 +0800

    Initial commit
    Start new MAUI application

---
+++ MyTasks/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyTasks
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MyTasks/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MyTasks
+{
+    [Register( "AppDelegate" )]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
